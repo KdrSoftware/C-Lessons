77cf5dd4de65e3cb472a59f1ac13cc43765da816
KdrSoftware <54094909+KdrSoftware@users.noreply.github.com>
2022-03-02 23:34:43 +0300

    Get and Display
    Get and Display

---
+++ get and display.cpp
@@ -0,0 +1,7 @@
+#include<iostream> //Loading Library
+using namespace std; 
+int main(){
+	string name; //String Variable
+	cin>>name; //Get name
+	cout<<"Welcome "+name; //Display Welcome ... name
+}
